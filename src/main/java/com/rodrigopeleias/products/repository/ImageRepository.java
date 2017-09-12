377519029cd91829ba186a5c73a7fc2b9cfd89da
Rodrigo Peleias <rodrigo.peleias@preventsenior.com.br>
2017-09-12 10:41:32 -0300

    Added queries on ProductRepository

--- src/main/java/com/rodrigopeleias/products/repository/ImageRepository.java
+++ src/main/java/com/rodrigopeleias/products/repository/ImageRepository.java
@@ -1,9 +1,14 @@
 package com.rodrigopeleias.products.repository;
 
 import com.rodrigopeleias.products.domain.Image;
+import com.rodrigopeleias.products.domain.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ImageRepository extends JpaRepository<Image, Long>{
+
 }
--- src/main/java/com/rodrigopeleias/products/repository/ProductRepository.java
+++ src/main/java/com/rodrigopeleias/products/repository/ProductRepository.java
@@ -2,9 +2,21 @@ package com.rodrigopeleias.products.repository;
 
 import com.rodrigopeleias.products.domain.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    @Query("select p from Product p join fetch p.images im")
+    List<Product> findAllWithImages();
+
+    @Query("select p from Product p join fetch p.parentProduct p")
+    List<Product> findAllWithParentProduct();
+
+    @Query("select p from Product p join fetch p.parentProduct p join fetch p.images im")
+    List<Product> findAllWithImagesAndParentProduct();
 
 }
